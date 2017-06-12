c851d2c6b4fe242d20044358ade92f28ee4e89af
MohamedMagDi7 <mohamedmega21@gmail.com>
2017-06-12 15:48:55 +0200

    Updated Revel Commit

--- tests/apptest.go
+++ tests/apptest.go
@@ -2,6 +2,9 @@ package tests
 
 import (
 	"github.com/revel/revel/testing"
+
+	"net/url"
+
 )
 
 type AppTest struct {
@@ -20,6 +23,13 @@ func (t *AppTest) TestIndexPage() {
 
 func (t *AppTest) TestUserPage() {
 	t.Get("/userpage")
+	t.AssertOk()
+	t.AssertContentType("text/html; charset=utf-8")
+}
+
+func (t *AppTest) TestLoginFunc() {
+
+	t.PostForm("/login",url.Values{"username": {"mohamedmega21@gmail.com" } , "password":{"1819"} , "login":{"Log In"}})
 	t.AssertOk()
 	t.AssertContentType("text/html; charset=utf-8")
 }
